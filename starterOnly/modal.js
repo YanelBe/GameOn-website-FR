43c0e463b3ab374299b1e4d9471e91ccb5b08673
Yanel <yanel.bensa@gmail.com>
2023-03-30 16:51:13 +0200

    Avancée sur la vérification du formulaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,8 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const submitBtn = document.querySelector(".btn-submit");
+const formDisplay = document.getElementsByName("reserve");
 
 // Variable du bouton de fermeture de modale
 const modalBtnClose = document.querySelector(".close");
@@ -21,7 +23,9 @@ const inputLastName = document.getElementById("last");
 const inputEmail = document.getElementById("email");
 const inputBirthdate = document.getElementById("birthdate");
 const inputQuantity = document.getElementById("quantity");
-const inputLocation = document.getElementsByName("location");
+const locationRadio = document.getElementsByName("location");
+const termsOfUseCheckbox = document.getElementById("checkbox1");
+const successForm = document.getElementById("success");
 
 
 // Variables pour les messages d'erreur
@@ -37,9 +41,38 @@ const errorData = document.querySelectorAll("formData");
 
 // RegEx 
 const regExName = new RegExp(/^[a-zA-Zà-üÀ-Ü]{2,32}$/);
-const regExMail = new RegExp("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$");
+const regExMail = new RegExp(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
+const regExBirthdate = new RegExp(/^((19[0-9]+[0-9]|20[0-1]+[0-9])-(0?[1-9]|1[0-2])-(0?[1-9]|[12]\d|3[01])|(0?[1-9]|[12]\d|3[01])[/](0?[1-9]|1[0-2])[/](19[3-9]+[0-9]|200[0-6]))$/);
+const regExQuantity = new RegExp(/^([0-9]){1,2}$/);
 
-// Fonctions pour appliquer des conditions de validation du formulaire à l'aide des RegEx
+/* Ouverture & Fermeture de la Modale */
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// Ecoute de l'event pour fermer la modale
+modalBtnClose.addEventListener("click", closeModal);
+
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
+
+// Fonction pour fermer la modale
+function closeModal() {
+  // On rajoute une classe content-off pour activer l'animation css de fermeture de modale
+  modalbg.classList.add("content-off");
+  modalbg.addEventListener("animationend", function() {
+    modalbg.style.display = "none";
+    // On supprime la classe rajoutée, pour que l'animation de fermeture ne se déclenche pas
+    // si on souhaite ouvrir de nouveau la modale
+    modalbg.classList.remove("content-off");
+  }, {
+    once: true
+  });
+}
+
+
+/* Fonctions pour appliquer des conditions de validation du formulaire à l'aide des RegEx */
 
 // Fonction pour vérifier le champ du prénom
 function firstNameTest() {
@@ -89,28 +122,76 @@ function birthdateTest() {
 }
 birthdateTest();
 
-
-
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-// Ecoute de l'event pour fermer la modale
-modalBtnClose.addEventListener("click", closeModal);
-
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
+// Fonction pour vérifier si le nombre de tournois est indiqué
+function quantityTest() {
+  inputQuantity.addEventListener("change", () => {
+    if (regExQuantity.test(quantity.value)) {
+      quantityErrorMsg.textContent = "";
+    } else {
+      quantityErrorMsg.textContent = "Veuillez entrer un nombre valide.";
+    }
+  });
 }
+quantityTest();
 
-// Fonction pour fermer la modale
-function closeModal() {
-  // On rajoute une classe content-off pour activer l'animation de fermeture de modale
-  modalbg.classList.add("content-off");
-  modalbg.addEventListener("animationend", function() {
-    modalbg.style.display = "none";
-    modalbg.classList.remove("content-off");
-  }, {
-    once: true
+// Fonction pour vérifier si une ville est bien sélectionnée 
+function locationTest() {
+  // On définit une variable pour définir l'état des cases, à 0 par défaut
+  let radioCheck = 0;
+  // On définit une variable pour vérifier qu'une case est cochée (supérieur à 0)
+  const isRadioChecked = radioCheck > 0;
+  
+  // Pour toute case cochée, on augmente la quantité de case cochée (limitée à 1)
+  for (let i = 0; i < locationRadio.length; i++) {
+    if (locationRadio[i].checked) {
+      radioCheck++;
+    }
+  }
+  locationRadio.addEventListener("click", () => {
+    // Si une case n'est pas cochée, on affiche un message d'erreur
+    if (isRadioChecked) {
+      locationErrorMsg.textContent = "";
+    } else {
+      locationErrorMsg.textContent = "Veuillez sélectionner une ville.";
+    }
   });
 }
 
+//Fonction pour vérifier si la case des conditions d'utilisation est acceptée
+function termsOfUseTest() {
+  const isCheckboxValid = termsOfUseCheckbox.checked;
+
+  termsOfUseCheckbox.addEventListener("click", () => { 
+    if (isCheckboxValid) {
+      termsOfUseErrorMsg.textContent = "";
+    } else {
+      termsOfUseErrorMsg.textContent = "Veuillez accepter les conditions d'utilisation."
+    }
+  });
+}
+
+//Fonction pour valider le formulaire
+function validate(event) {
+  event.preventDefault();
+  submitBtn.addEventListener("submit", () => {
+
+    const isFormValid = firstNameTest() && lastNameTest() && emailTest() && birthdateTest() 
+                        && quantityTest() && locationTest() && termsOfUseTest();
+
+    firstNameTest();
+    lastNameTest();
+    emailTest();
+    birthdateTest();
+    quantityTest();
+    locationTest();
+    termsOfUseTest(); 
+
+   if (isFormValid) {
+      formDisplay.style.display = "none";
+      successForm.style.display = "block";
+      successForm.textContent = "Merci ! Votre réservation a été reçue.";
+    } 
+  });
+}
+
+validate();
