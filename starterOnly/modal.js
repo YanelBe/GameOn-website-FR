1279e0c90996bbe8b503e72b6aae8dde0ccf03d0
Yanel <yanel.bensa@gmail.com>
2023-04-15 21:09:51 +0200

    Correction de l'interface

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,8 +11,8 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-const submitBtn = document.querySelector(".btn-submit");
-const formDisplay = document.getElementsByName("reserve");
+const closeBtn = document.querySelector(".btn-close");
+const myForm = document.getElementById("myForm");
 
 // Variable du bouton de fermeture de modale
 const modalBtnClose = document.querySelector(".close");
@@ -30,17 +30,24 @@ const successForm = document.getElementById("success");
 
 // Variables pour les messages d'erreur
 const firstNameErrorMsg = document.getElementById("firstNameErrorMsg");
+const nameErrorMsgContent = "Veuillez entrer entre 2 et 32 caractères (lettres, espaces et tirets uniquement)";
 const lastNameErrorMsg = document.getElementById("lastNameErrorMsg");
+const birthdateErrorMsg = document.getElementById("birthdateErrorMsg");
+const birthdateErrorMsgContent = "Veuillez entrer une date de naissance valide.";
 const emailErrorMsg = document.getElementById("emailErrorMsg");
+const emailErrorMsgContent = "Veuillez entrer une date de naissance valide.";
 const quantityErrorMsg = document.getElementById("quantityErrorMsg");
+const quantityErrorMsgContent = "Veuillez entrer un nombre valide (entre 0 et 99).";
 const locationErrorMsg = document.getElementById("locationErrorMsg");
+const locationErrorMsgContent = "Veuillez sélectionner une ville.";
 const termsOfUseErrorMsg = document.getElementById("termsOfUseErrorMsg");
+const termsOfUseErrorMsgContent = "Veuillez accepter les conditions d'utilisation.";
 const errorData = document.querySelectorAll("formData");
 
 
 
 // RegEx 
-const regExName = new RegExp(/^[a-zA-Zà-üÀ-Ü]{2,32}$/);
+const regExName = new RegExp(/^(?=.{2,32}$)[a-zA-ZÀ-ÿ]+(?:[ -][a-zA-ZÀ-ÿ]+)*$/);
 const regExMail = new RegExp(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
 const regExBirthdate = new RegExp(/^((19[0-9]+[0-9]|20[0-1]+[0-9])-(0?[1-9]|1[0-2])-(0?[1-9]|[12]\d|3[01])|(0?[1-9]|[12]\d|3[01])[/](0?[1-9]|1[0-2])[/](19[3-9]+[0-9]|200[0-6]))$/);
 const regExQuantity = new RegExp(/^([0-9]){1,2}$/);
@@ -61,7 +68,7 @@ function launchModal() {
 function closeModal() {
   // On rajoute une classe content-off pour activer l'animation css de fermeture de modale
   modalbg.classList.add("content-off");
-  modalbg.addEventListener("animationend", function() {
+  modalbg.addEventListener("animationend", () => {
     modalbg.style.display = "none";
     // On supprime la classe rajoutée, pour que l'animation de fermeture ne se déclenche pas
     // si on souhaite ouvrir de nouveau la modale
@@ -69,129 +76,114 @@ function closeModal() {
   }, {
     once: true
   });
+
+  /* Si le formulaire est caché (le formulaire se cache pour laisser place au message de confirmation, voir
+     la fonction formValidate() plus bas), le bouton de fermeture de modale va réinitialiser les input du formulaire
+     et le faire réapparaitre. On ajoute un setTimeout pour que le formulaire ne réapparaisse pas pendant l'animation
+     de fermeture de la modale */
+  if (myForm.classList.contains("hidden")) { 
+    setTimeout(() => {
+      myForm.classList.remove("hidden");
+      successForm.classList.add("hidden");
+      myForm.reset();
+    }, 800);
+  }
 }
 
-
-/* Fonctions pour appliquer des conditions de validation du formulaire à l'aide des RegEx */
-
-// Fonction pour vérifier le champ du prénom
-function firstNameTest() {
-  inputFirstName.addEventListener("change", () => {
-      if (regExName.test(first.value)) {
-          firstNameErrorMsg.textContent = "";
-      } else {
-          firstNameErrorMsg.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
-      }
-  });
+/* Fonction pour la validation des différents champs du formulaire. La fonction prend 4 paramètres différents :
+  - field, pour le champ à valider (nom, prénom, etc)
+  - regex, la fonction pour valider le champ avec les regex
+  - errorMsg, qui indique où afficher le message d'erreur
+  - errorMsgContent, qui indiquera quoi afficher comme message d'erreur */
+function fieldValidate(field, regex, errorMsg, errorMsgContent) {
+  if (regex.test(field.value)) {
+    // Si le champ testé est valide, il n'y a pas de message
+    errorMsg.textContent = "";
+    field.style.border = "";
+  } else {
+    // Si le champ testé n'est pas valide, on affiche un message d'erreur
+    errorMsg.textContent = errorMsgContent;
+    field.style.border = "#fe142f 2px solid";
+    isFormValid = false;
+  }
 }
-firstNameTest();
 
-// Fonction pour vérifier le champ du nom
-function lastNameTest() {
-  inputLastName.addEventListener("change", () => {
-      if (regExName.test(last.value)) {
-          lastNameErrorMsg.textContent = "";
-      } else {
-          lastNameErrorMsg.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
-      }
-  });
-}
-lastNameTest();
+//Validation du champ du prénom
+inputFirstName.addEventListener("change", () => {
+  fieldValidate(inputFirstName, regExName, firstNameErrorMsg, nameErrorMsgContent);
+});
+//Validation du champ du nom
+inputLastName.addEventListener("change", () => {
+  fieldValidate(inputLastName, regExName, lastNameErrorMsg, nameErrorMsgContent);
+});
+  //Validation du champ de l'adresse email
+inputEmail.addEventListener("change", () => {
+  fieldValidate(inputEmail, regExMail, emailErrorMsg, emailErrorMsgContent);
+});
+//Validation du champ de la date de naissance
+inputBirthdate.addEventListener("change", () => {
+  fieldValidate(inputBirthdate, regExBirthdate, birthdateErrorMsg, birthdateErrorMsgContent);
+});
+//Validation du champ de la quantité d'évènements déjà visités
+inputQuantity.addEventListener("change", () => {
+  fieldValidate(inputQuantity, regExQuantity, quantityErrorMsg, quantityErrorMsgContent);
+});
 
-// Fonction pour vérifier le champ de l'adresse email
-function emailTest() {
-  inputEmail.addEventListener("change", () => {
-    if (regExMail.test(email.value)) {
-      emailErrorMsg.textContent = "";
-    } else {
-      emailErrorMsg.textContent = "Veuillez entrer une adresse email valide.";
-    }
-  });
-}
-emailTest();
+//Fonction pour valider le formulaire
+function formValidate(event) {
+  event.preventDefault();
 
-// Fonction pour vérifier le champ de la date de naissance
-function birthdateTest() {
-  inputBirthdate.addEventListener("change", () => {
-    if (regExBirthdate.test(birthdate.value)) {
-      birthdateErrorMsg.textContent = "";
-    } else {
-      birthdateErrorMsg.textContent = "Veuillez entrer une date de naissance valide.";
-    }
-  });
-}
-birthdateTest();
+  let isFormValid = true;
 
-// Fonction pour vérifier si le nombre de tournois est indiqué
-function quantityTest() {
-  inputQuantity.addEventListener("change", () => {
-    if (regExQuantity.test(quantity.value)) {
-      quantityErrorMsg.textContent = "";
-    } else {
-      quantityErrorMsg.textContent = "Veuillez entrer un nombre valide.";
-    }
-  });
-}
-quantityTest();
+  //Validation du champ du prénom
+  fieldValidate(inputFirstName, regExName, firstNameErrorMsg, nameErrorMsgContent);
+  //Validation du champ du nom
+  fieldValidate(inputLastName, regExName, lastNameErrorMsg, nameErrorMsgContent);
+  //Validation du champ de l'adresse email
+  fieldValidate(inputEmail, regExMail, emailErrorMsg, emailErrorMsgContent);
+  //Validation du champ de la date de naissance
+  fieldValidate(inputBirthdate, regExBirthdate, birthdateErrorMsg, birthdateErrorMsgContent);
+  //Validation du champ de la quantité d'évènements déjà visités
+  fieldValidate(inputQuantity, regExQuantity, quantityErrorMsg, quantityErrorMsgContent);
 
-// Fonction pour vérifier si une ville est bien sélectionnée 
-function locationTest() {
-  // On définit une variable pour définir l'état des cases, à 0 par défaut
+
+  //Validation de la sélection de la ville
   let radioCheck = 0;
-  // On définit une variable pour vérifier qu'une case est cochée (supérieur à 0)
-  const isRadioChecked = radioCheck > 0;
-  
-  // Pour toute case cochée, on augmente la quantité de case cochée (limitée à 1)
   for (let i = 0; i < locationRadio.length; i++) {
     if (locationRadio[i].checked) {
       radioCheck++;
     }
   }
-  locationRadio.addEventListener("click", () => {
-    // Si une case n'est pas cochée, on affiche un message d'erreur
-    if (isRadioChecked) {
-      locationErrorMsg.textContent = "";
-    } else {
-      locationErrorMsg.textContent = "Veuillez sélectionner une ville.";
-    }
-  });
-}
+  const isRadioChecked = radioCheck > 0;
+  if (isRadioChecked) {
+    locationErrorMsg.textContent = "";
+  } else {
+    locationErrorMsg.textContent = locationErrorMsgContent;
+    isFormValid = false;
+  }
 
-//Fonction pour vérifier si la case des conditions d'utilisation est acceptée
-function termsOfUseTest() {
+  //Validation de validation des conditions d'utilisation
   const isCheckboxValid = termsOfUseCheckbox.checked;
+  if (isCheckboxValid) {
+    termsOfUseErrorMsg.textContent = "";
+  } else {
+    termsOfUseErrorMsg.textContent = termsOfUseErrorMsgContent;
+    isFormValid = false;
+  }
 
-  termsOfUseCheckbox.addEventListener("click", () => { 
-    if (isCheckboxValid) {
-      termsOfUseErrorMsg.textContent = "";
-    } else {
-      termsOfUseErrorMsg.textContent = "Veuillez accepter les conditions d'utilisation."
-    }
-  });
-}
+  //Si tout est valide, on cache le formulaire
+  if (isFormValid) {
+    myForm.classList.add("hidden");
+    successForm.classList.remove("hidden");
+  }
+};
 
-//Fonction pour valider le formulaire
-function validate(event) {
-  event.preventDefault();
-  submitBtn.addEventListener("submit", () => {
+//On écoute le submit du formulaire, auquel on applique la fonction formValidate
+myForm.addEventListener("submit", formValidate);
 
-    const isFormValid = firstNameTest() && lastNameTest() && emailTest() && birthdateTest() 
-                        && quantityTest() && locationTest() && termsOfUseTest();
+//On écoute l'évènement clic sur le bouton "Fermer" de la modale (qui s'affiche si le formulaire est bien envoyé)
+closeBtn.addEventListener("click", () => {
+  closeModal()
+})
 
-    firstNameTest();
-    lastNameTest();
-    emailTest();
-    birthdateTest();
-    quantityTest();
-    locationTest();
-    termsOfUseTest(); 
 
-   if (isFormValid) {
-      formDisplay.style.display = "none";
-      successForm.style.display = "block";
-      successForm.textContent = "Merci ! Votre réservation a été reçue.";
-    } 
-  });
-}
-
-validate();
